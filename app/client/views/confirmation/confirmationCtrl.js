498897f9974c716efaaad1428a9f7e7d8660b129
neilmckibben-osu <mckibben.64@osu.edu>
2019-08-26 17:53:56 -0400

    Clarified laptop prompt and added verification

--- app/client/views/confirmation/confirmationCtrl.js
+++ app/client/views/confirmation/confirmationCtrl.js
@@ -81,6 +81,15 @@ angular.module('reg')
                 }
               ]
             },
+            wantsLaptop: {
+              identifier: 'wantsLaptop',
+              rules: [
+                {
+                  type: 'empty',
+                  prompt: 'Please give us a laptop preference!'
+                }
+              ]
+            },
             phone: {
               identifier: 'phone',
               rules: [
