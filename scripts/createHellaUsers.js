27692c5dbb07ea65923e26832a96e32048518243
Parshva Shah <pkshah2017@gmail.com>
2020-01-22 09:39:34 -0500

    Update scripts
    Update createHellaUser's script to include error logging
    Created a getUserCSV script which generates a CSV of all confirmed students.

--- scripts/createHellaUsers.js
+++ scripts/createHellaUsers.js
@@ -1,7 +1,7 @@
 // Connect to mongodb
 var mongoose        = require('mongoose');
-var database        = process.env.DATABASE || { url: "mongodb://localhost:27017"};
-mongoose.connect(database.url);
+var database        = process.env.DATABASE || "mongodb://localhost:27017";
+mongoose.connect(database);
 
 var UserController = require('../app/server/controllers/UserController');
 
@@ -11,7 +11,7 @@ var username = 'hacker';
 for (var i = 0; i < users; i++){
   console.log(username, i);
   UserController
-    .createUser(username + i + '@school.edu', 'foobar', function(){
-    console.log(i);
+    .createUser(username + i + '@school.edu', 'foobar', function(err, {token, user}){
+      if (err) console.log(err);      
     });
 }
