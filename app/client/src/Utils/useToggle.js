ff152fd9a673877aa15675cbbaa9e6bcd08fe62f
Parshva Shah <pkshah2017@gmail.com>
2021-01-24 16:06:22 -0500

    Create useToggle.js
    Create a React Hook to toggle a value

---
+++ app/client/src/Utils/useToggle.js
@@ -0,0 +1,10 @@
+
+import { useReducer, Reducer } from 'react';
+
+const toggleReducer = (state, nextValue) => (typeof nextValue === 'boolean' ? nextValue : !state);
+
+const useToggle = (initialValue) => {
+    return useReducer(toggleReducer, initialValue);
+};
+
+export default useToggle;
