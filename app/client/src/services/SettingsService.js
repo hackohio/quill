4ff87594489254a95e95e9cc60ad1f7df7df1df1
Parshva Shah <pkshah2017@gmail.com>
2021-01-24 16:02:05 -0500

    Update SettingsService
    Update SettingsService to work without Angular and depend on axios

--- app/client/src/services/SettingsService.js
+++ app/client/src/services/SettingsService.js
@@ -1,54 +1,109 @@
-angular.module('reg')
-  .factory('SettingsService', [
-    '$http',
-    function ($http) {
+import axios from 'axios';
 
-      const base = '/api/settings/';
+const base = '/api/settings/';
 
-      return {
-        getPublicSettings: function () {
-          return $http.get(base);
-        },
-        updateRegistrationTimes: function (open, close) {
-          return $http.put(base + 'times', {
-            timeOpen: open,
-            timeClose: close,
-          });
-        },
-        updateConfirmationTime: function (time) {
-          return $http.put(base + 'confirm-by', {
-            time: time
-          });
-        },
-        getWhitelistedEmails: function () {
-          return $http.get(base + 'whitelist');
-        },
-        updateWhitelistedEmails: function (emails) {
-          return $http.put(base + 'whitelist', {
-            emails: emails
-          });
-        },
-        updateWaitlistText: function (text) {
-          return $http.put(base + 'waitlist', {
-            text: text
-          });
-        },
-        updateAcceptanceText: function (text) {
-          return $http.put(base + 'acceptance', {
-            text: text
-          });
-        },
-        updateConfirmationText: function (text) {
-          return $http.put(base + 'confirmation', {
-            text: text
-          });
-        },
-        updateAllowMinors: function (allowMinors) {
-          return $http.put(base + 'minors', {
-            allowMinors: allowMinors
-          });
-        },
-      };
+/**
+ * Returns a promise which resolves to the public settings
+ * @returns {Promise}
+ */
+function getPublicSettings() {
+  return axios.get(base);
+};
 
-    }
-  ]);
+/**
+ * Set the registration open and close times.
+ * @param {Number} open 
+ * @param {Number} close 
+ * @returns {Promise}
+ */
+function updateRegistrationTimes(open, close) {
+  return axios.put(base + 'times', {
+    timeOpen: open,
+    timeClose: close,
+  });
+};
+
+/**
+ * Update the confirmation date.
+ * @param {Number} time 
+ * @returns {Promise}
+ */
+function updateConfirmationTime(time) {
+  return axios.put(base + 'confirm-by', {
+    time: time
+  });
+};
+
+/**
+ * Get the whitelisted emails.
+ * @returns {Promise} Resolves to the whitelisted emails
+ */
+function getWhitelistedEmails() {
+  return axios.get(base + 'whitelist');
+};
+
+/**
+ * Updates the whitelisted emails to only those provided here
+ * @param {[String]} emails 
+ * @returns {Promise}
+ */
+function updateWhitelistedEmails(emails) {
+  return axios.put(base + 'whitelist', {
+    emails: emails
+  });
+};
+
+/**
+ * Update the waitlist text
+ * @param {String} text 
+ * @returns {Promise}
+ */
+function updateWaitlistText(text) {
+  return axios.put(base + 'waitlist', {
+    text: text
+  });
+};
+
+/**
+ * Update the acceptance text
+ * @param {String} text 
+ * @returns {Promise}
+ */
+function updateAcceptanceText(text) {
+  return axios.put(base + 'acceptance', {
+    text: text
+  });
+};
+
+/**
+ * Update the confirmation text
+ * @param {String} text 
+ * @returns {Promise}
+ */
+function updateConfirmationText(text) {
+  return axios.put(base + 'confirmation', {
+    text: text
+  });
+};
+
+/**
+ * Update the field to allow minors 
+ * @param {Boolean} allowMinors 
+ */
+function updateAllowMinors(allowMinors) {
+  return axios.put(base + 'minors', {
+    allowMinors: allowMinors
+  });
+};
+
+export default SettingsService = {
+  getPublicSettings,
+  updateRegistrationTimes,
+  updateConfirmationTime,
+  getWhitelistedEmails,
+  updateWhitelistedEmails,
+  updateWaitlistText,
+  updateAcceptanceText,
+  updateConfirmationText,
+  updateAllowMinors
+}
