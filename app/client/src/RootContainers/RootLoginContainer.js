9867ef6e214fe591fac7e3992f8c726b3d14fe69
egolfc <cae5511@gmail.com>
2021-02-03 21:03:42 -0500

    bare bones semantic ui login page

--- app/client/src/RootContainers/RootLoginContainer.js
+++ app/client/src/RootContainers/RootLoginContainer.js
@@ -1,5 +1,67 @@
 import React from 'react';
 
-const title = "Welcome to the Login Page";
+const title = "Login";
 
-export default () => { return (<div>{title}</div>); };
+const loginButtons=
+    <div className="ui container">
+        <div className ="ui divider">
+            <br/>
+            <div class="ui container">
+                <div className="ui buttons">
+                    <button className="ui red button">Login</button>
+                    <div className="or"></div>
+                    <button className="ui blue button">Register</button>
+                </div>
+                <a href=""> Forgot Password?</a>
+            </div>
+        </div>
+    </div>
+;
+
+const fields = 
+    <div className ="ui stackable grid">
+        <div className="fields">
+            <div className="field">
+                <lable>Email </lable>
+                <div className="ui mini icon input">
+                    <input type="text" placeholder="School Email" id="email-input"></input>
+                </div> 
+            </div>
+        </div>
+        <div className="fields">
+            <div className="field">
+            <lable>Password </lable>
+                <div className="ui mini icon input">
+                    <input type="password" placeholder="Password" id="user-password"></input>
+                </div> 
+            </div>
+        </div>
+
+    </div>
+;
+
+const loginModal =
+    <form className="ui form">
+        <div className="ui container">
+        <img className="ui centered small image" src="assets/images/logo-color.svg"/>
+            <div className="ui container">
+                <div className="ui divider"></div>
+                <br/>
+                {fields}
+            </div>
+        </div>
+        {loginButtons}
+    </form>
+;
+
+
+
+export default () => { 
+    return (
+        <div className = "ui main container">
+            <div className = "ui raised very padded text container segment">
+                {loginModal}
+            </div>
+        </div>
+    );   
+};
