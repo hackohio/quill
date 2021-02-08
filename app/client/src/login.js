d6eccedb171ee812e310e936461e726e81285e63
egolfc <cae5511@gmail.com>
2021-02-08 12:01:45 -0500

    Remove bad form validation

--- app/client/src/login.js
+++ app/client/src/login.js
@@ -12,35 +12,6 @@ const render = Component => {
 
 render(RootContainer);
 
-$('ui.form')
-    .form({
-        fileds:{
-            email:{
-                identifier: 'email',
-                rules:[
-                    {
-                        type: 'empty',
-                        prompt: 'Please enter your school email'
-                    }
-                ]
-            },
-            password:{
-                identifier: 'password',
-                rules:[
-                    {
-                        type    : 'empty',
-                        prompt  : 'Please enter a strong password'
-                    },
-                    {
-                        type    : 'minLength[6]',
-                        prompt  : 'Your password must be at least {ruleValue} characters'
-                    }
-                ]
-            }
-        }
-    })
-;
-
 if (module.hot) {
     module.hot.accept('./RootContainers/RootLoginContainer.js', () => {
         const RootContainer = require('./RootContainers/RootLoginContainer.js').default;
