92b781f1f633cd2e7b7703b8806ae006a77cb40f
egolfc <cae5511@gmail.com>
2021-02-09 11:03:23 -0500

    small ui changes to login container

--- app/client/src/RootContainers/RootLoginContainer.js
+++ app/client/src/RootContainers/RootLoginContainer.js
@@ -32,54 +32,57 @@ const RootLoginContainer = () => {
                 ></Form.Input>
               </Form.Field>
               {forgotPassword ? (
-                <Button fluid circular animated color="green" onClick={() => {
-                setForgotPassword(false);}
-                  }>
-                      <Button.Content visible> Send Reset Email</Button.Content>
-                      <Button.Content hidden>
-                          <Icon name="mail"></Icon>
-                      </Button.Content>
-                </Button>     
+                <Button
+                  fluid
+                  circular
+                  animated
+                  color="green"
+                  onClick={() => {
+                    setForgotPassword(false);
+                  }}
+                >
+                  <Button.Content visible> Send Reset Email</Button.Content>
+                  <Button.Content hidden>
+                    <Icon name="mail"></Icon>
+                  </Button.Content>
+                </Button>
               ) : (
                 <>
-                <Form.Field>
-                  <Form.Input
-                    placeholder="Password"
-                    label="Password"
-                    name="password"
-                  ></Form.Input>
-                </Form.Field>
-                <Form.Group inline>
+                  <Form.Field>
+                    <Form.Input
+                      placeholder="Password"
+                      label="Password"
+                      name="password"
+                    ></Form.Input>
+                  </Form.Field>
+                  <Form.Group inline>
                     <Button fluid circular color="red">
-                        Login
+                      Login
                     </Button>
                     <Button fluid circular color="blue">
-                        Register
-                    </Button>          
-                </Form.Group>
-              </>
+                      Register
+                    </Button>
+                  </Form.Group>
+                </>
               )}
             </Form>
             <Divider />
             {forgotPassword ? (
-                <>
-                </>
-            ):(
-            <Form>
-              <Button
-                fluid
-                circular
-                color="grey"
-                onClick={() => {
-                  setForgotPassword(true);
-                }}
-              >
-                Forgot Password?
-              </Button>
-            </Form>
-
+              <></>
+            ) : (
+              <Form>
+                <Button
+                  fluid
+                  circular
+                  color="grey"
+                  onClick={() => {
+                    setForgotPassword(true);
+                  }}
+                >
+                  Forgot Password?
+                </Button>
+              </Form>
             )}
-            
           </Segment>
         </Grid.Column>
       </Grid>
