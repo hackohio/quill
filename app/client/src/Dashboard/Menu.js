a137723510f67d68f76da984c7d8ab4a4c911180
egolfc <cae5511@gmail.com>
2021-03-20 19:11:40 -0400

    start react router setup

--- app/client/src/Dashboard/Menu.js
+++ app/client/src/Dashboard/Menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
 export default class MenuExampleVerticalSecondary extends Component {
@@ -11,31 +12,41 @@ export default class MenuExampleVerticalSecondary extends Component {
 
     return (
       <Menu color="red" pointing secondary vertical>
-        <Menu.Item
-          name="Dashboard"
-          active={activeItem === "dashboard"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="Application"
-          active={activeItem === "application"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="Confirmation"
-          active={activeItem === "confirmation"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="Team"
-          active={activeItem === "team"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name="Logout"
-          active={activeItem === "logout"}
-          onClick={this.handleItemClick}
-        />
+        <Link to="/dashboard">
+          <Menu.Item
+            name="Dashboard"
+            active={activeItem === "dashboard"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
+        <Link to="/application">
+          <Menu.Item
+            name="Application"
+            active={activeItem === "application"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
+        <Link to="/confirmation">
+          <Menu.Item
+            name="Confirmation"
+            active={activeItem === "confirmation"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
+        <Link to="/team">
+          <Menu.Item
+            name="Team"
+            active={activeItem === "team"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
+        <Link to="/logout">
+          <Menu.Item
+            name="Logout"
+            active={activeItem === "logout"}
+            onClick={this.handleItemClick}
+          />
+        </Link>
       </Menu>
     );
   }
