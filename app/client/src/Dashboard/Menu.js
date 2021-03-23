2ba98ccf09430fbb41652e40baf2f4769d7748e1
Parshva Shah <pkshah2017@gmail.com>
2021-03-23 14:31:42 -0400

    Update Menu.js to a functional component

--- app/client/src/Dashboard/Menu.js
+++ app/client/src/Dashboard/Menu.js
@@ -1,69 +1,69 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Menu, Container } from "semantic-ui-react";
+import React, { Component, useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Menu, Container } from 'semantic-ui-react';
 
-export default class MenuExampleVerticalSecondary extends Component {
-  state = { activeItem: "home" };
+export default function MenuExampleVerticalSecondary() {
+  const [activeItem, setActiveitem] = useState('home');
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  handleItemClick = useCallback((e, { name }) => setActiveitem(name), [
+    setActiveitem,
+  ]);
 
-  render() {
-    const { activeItem } = this.state;
-    const submitted = true;
-    const verified = true;
-    return (
-      <Menu color="red" pointing secondary vertical>
-        <Link to="/dashboard">
-          <Menu.Item
-            name="Dashboard"
-            active={activeItem === "dashboard"}
-            onClick={this.handleItemClick}
-          />
-        </Link>
-        {submitted ? (
-          <Container>
-            <Link to="/application">
-              <Menu.Item
-                name="Application"
-                active={activeItem === "application"}
-                onClick={this.handleItemClick}
-              />
-            </Link>
-            <Link to="/confirmation">
-              <Menu.Item
-                name="Confirmation"
-                active={activeItem === "confirmation"}
-                onClick={this.handleItemClick}
-              />
-            </Link>
-            <Link to="/team">
-              <Menu.Item
-                name="Team"
-                active={activeItem === "team"}
-                onClick={this.handleItemClick}
-              />
-            </Link>
-          </Container>
-        ) : verified ? (
+  const submitted = true;
+  const verified = true;
+
+  return (
+    <Menu color="red" pointing secondary vertical>
+      <Link to="/dashboard">
+        <Menu.Item
+          name="Dashboard"
+          active={activeItem === 'dashboard'}
+          onClick={this.handleItemClick}
+        />
+      </Link>
+      {submitted ? (
+        <Container>
           <Link to="/application">
             <Menu.Item
               name="Application"
-              active={activeItem === "application"}
+              active={activeItem === 'application'}
               onClick={this.handleItemClick}
             />
           </Link>
-        ) : (
-          <div />
-        )}
-
-        <Link to="/logout">
+          <Link to="/confirmation">
+            <Menu.Item
+              name="Confirmation"
+              active={activeItem === 'confirmation'}
+              onClick={this.handleItemClick}
+            />
+          </Link>
+          <Link to="/team">
+            <Menu.Item
+              name="Team"
+              active={activeItem === 'team'}
+              onClick={this.handleItemClick}
+            />
+          </Link>
+        </Container>
+      ) : verified ? (
+        <Link to="/application">
           <Menu.Item
-            name="Logout"
-            active={activeItem === "logout"}
+            name="Application"
+            active={activeItem === 'application'}
             onClick={this.handleItemClick}
           />
         </Link>
-      </Menu>
-    );
-  }
+      ) : (
+        <div />
+      )}
+
+      <Link to="/logout">
+        <Menu.Item
+          name="Logout"
+          active={activeItem === 'logout'}
+          onClick={this.handleItemClick}
+        />
+      </Link>
+    </Menu>
+  );
 }
