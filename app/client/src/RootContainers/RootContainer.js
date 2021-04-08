eb5d932b19ca13cca6e859e07e96bb53aa976928
egolfc <cae5511@gmail.com>
2021-04-08 10:14:15 -0400

    created out admin stats shell.

--- app/client/src/RootContainers/RootContainer.js
+++ app/client/src/RootContainers/RootContainer.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Grid, Container, Divider, Image } from "semantic-ui-react";
-import DashMenu from "../Dashboard/Menu";
-import DashWindow from "../Dashboard/DashWindow";
-import ApplicationForm from "../Application/Application";
-import ConfirmationForm from "../Confirmation/confirmation";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Grid, Container, Divider, Image } from 'semantic-ui-react';
+import DashMenu from '../Dashboard/Menu';
+import DashWindow from '../Dashboard/DashWindow';
+import ApplicationForm from '../Application/Application';
+import ConfirmationForm from '../Confirmation/confirmation';
+import AdminBoard from '../Admin/Admin';
 
 const DashboardDefault = () => (
   <div>
@@ -27,6 +28,9 @@ const DashboardDefault = () => (
             </Route>
             <Route path="/confirmation">
               <ConfirmationForm />
+            </Route>
+            <Route path="/admin">
+              <AdminBoard />
             </Route>
             <Route path="/team"></Route>
             <Route path="/logout"></Route>
