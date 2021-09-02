83396535f2cb70cae03d6b8846c3330fde002c8f
egolfc <cae5511@gmail.com>
2021-09-02 11:47:42 -0400

    removed lottery wording

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -7,11 +7,11 @@ angular.module('reg')
     .constant('DASHBOARD', {
         UNVERIFIED: 'You should have received an email asking you verify your email. Click the link in the email and you can start your application! If you can\'t find your verification email, please check your spam folder.',
         INCOMPLETE_TITLE: 'You still need to complete your application!',
-        INCOMPLETE: 'If you do not complete your application before [APP_DEADLINE], you will not be considered for the admissions lottery!',
+        INCOMPLETE: 'Please complete your application before [APP_DEADLINE].',
         SUBMITTED_TITLE: 'Your application has been submitted!',
-        SUBMITTED: 'Feel free to edit it at any time. However, once registration is closed, you will not be able to edit it any further.\nAdmissions will be determined by a random lottery, if we reach capacity. Please make sure your information is accurate before registration is closed!',
-        CLOSED_AND_INCOMPLETE_TITLE: 'Unfortunately, registration has closed, and the lottery process has begun.',
-        CLOSED_AND_INCOMPLETE: 'Because you have not completed your profile in time, you will not be eligible for the lottery process.',
+        SUBMITTED: 'Feel free to edit it at any time. However, once registration is closed, you will not be able to edit it any further.\nPlease make sure your information is accurate before registration is closed!',
+        CLOSED_AND_INCOMPLETE_TITLE: 'Unfortunately, registration has closed.',
+        CLOSED_AND_INCOMPLETE: 'Sorry, you did not complete your application and registration is closed',
         ADMITTED_AND_CAN_CONFIRM_TITLE: 'You must confirm by [CONFIRM_DEADLINE].',
         ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE] has passed.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
@@ -19,5 +19,5 @@ angular.module('reg')
         DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to HackOHI/O 2021! :(\nMaybe next year! We hope you see you again soon.',
     })
     .constant('TEAM', {
-        NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter the lottery with a team.\nHowever, you can still form teams on your own before or during the event!',
+        NO_TEAM_REG_CLOSED: 'Unfortunately, it\'s too late to enter with a team.\nHowever, you can still form teams on your own before or during the event!',
     });
